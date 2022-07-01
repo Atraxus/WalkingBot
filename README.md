884f954a5ed85660b940a1de93c697135cc911c8
Jannis Maier <jannismaier97@gmail.com>
2022-07-01 19:57:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WalkingBot
+ WalkingBot is a project for university. We are training a bot to walk through an evolution process.
---
+++ controllers/WalkingBot/WalkingBot.py
@@ -0,0 +1,14 @@
+"""WalkingBot controller."""
+
+# You may need to import some classes of the controller module. Ex:
+#  from controller import Robot, Motor, DistanceSensor
+from controller import Robot
+
+# create the Robot instance.
+robot = Robot()
+
+# get the time step of the current world.
+timestep = int(robot.getBasicTimeStep())
+
+while robot.step(32) != -1:
+    print("Hello World!")
