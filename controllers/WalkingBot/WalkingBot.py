5ba3ed7bdf24c87303887e4baf7a5c934c133820
Jannis Maier <jannismaier97@gmail.com>
2022-07-02 17:11:53 +0200

    trying to get legs from robot in controller

--- controllers/WalkingBot/WalkingBot.py
+++ controllers/WalkingBot/WalkingBot.py
@@ -1,12 +1,20 @@
-from controller import Robot, DistanceSensor
+from controller import Robot
 
+# Global defines
 TIME_STEP = 32
 
+# Robot initialization
 robot = Robot()
+leftArm = robot.getDevice('PRM:/r1/c1/c2-Joint2:12')
+rightArm = robot.getDevice("RighArmJoint")
+print(leftArm)
+print(rightArm)
 
+# Sensors
 accelerometer = robot.getDevice("accelerometer")
 accelerometer.enable(TIME_STEP)
 
+# Main Loop
 while robot.step(TIME_STEP) != -1:
     value = accelerometer.getValues()
     print("Sensor value is: ", value)
