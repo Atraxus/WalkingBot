aac3543b88251afd862c4fb29739d25f0b1b15ff
Jannis Maier <jannismaier97@gmail.com>
2022-07-03 10:48:58 +0200

    added motors to robot

--- controllers/WalkingBot/WalkingBot.py
+++ controllers/WalkingBot/WalkingBot.py
@@ -5,10 +5,24 @@ TIME_STEP = 32
 
 # Robot initialization
 robot = Robot()
-leftArm = robot.getDevice('PRM:/r1/c1/c2-Joint2:12')
-rightArm = robot.getDevice("RighArmJoint")
-print(leftArm)
-print(rightArm)
+
+# For testing purposes
+numDevices = robot.getNumberOfDevices()
+print("Number of devices:", numDevices)
+for i in range(numDevices):
+    print("Device", i, ":", robot.getDeviceByIndex(i).getName())
+
+
+# Motors
+leftMotor1 = robot.getDevice("LeftLegtMotor1")
+leftMotor2 = robot.getDevice("LeftLegtMotor2")
+rightMotor1 = robot.getDevice("RightLegtMotor1")
+rightMotor2 = robot.getDevice("RightLegtMotor2")
+leftMotor1.setVeloctity(0.0)
+leftMotor2.setVeloctity(0.0)
+rightMotor1.setVeloctity(0.0)
+rightMotor2.setVeloctity(0.0)
+
 
 # Sensors
 accelerometer = robot.getDevice("accelerometer")
