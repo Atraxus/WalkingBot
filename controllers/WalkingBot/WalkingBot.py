0ce6d28e9c3057f1878d799d2b7ee2f1de56aa14
Jannis Maier <jannismaier97@gmail.com>
2022-07-04 17:19:17 +0200

    parameter experimentation

--- controllers/WalkingBot/WalkingBot.py
+++ controllers/WalkingBot/WalkingBot.py
@@ -1,7 +1,8 @@
-from controller import Robot
+from controller import Robot, Device, Node, Motor, Accelerometer
 
 # Global defines
 TIME_STEP = 32
+yTarget = 1
 
 # Robot initialization
 robot = Robot()
@@ -19,11 +20,28 @@ leftMotor2 = robot.getDevice("LeftLegtMotor2")
 rightMotor1 = robot.getDevice("RightLegtMotor1")
 rightMotor2 = robot.getDevice("RightLegtMotor2")
 
+# Set PID gains
+leftMotor1.setControlPID(100, 10, 0.1)
+leftMotor2.setControlPID(100, 10, 0.1)
+rightMotor1.setControlPID(100, 10, 0.1)
+rightMotor2.setControlPID(100, 10, 0.1)
+
 # Sensors
 accelerometer = robot.getDevice("accelerometer")
 accelerometer.enable(TIME_STEP)
 
-# Main Loop
+# Main Loop to get the robot to walk
 while robot.step(TIME_STEP) != -1:
-    value = accelerometer.getValues()
-    #print("Sensor value is: ", value)
+    # Get robot y position
+    # y = accelerometer.getValues()[1]
+    # Calculate error
+    # error = abs(yTarget - y)
+    # Calculate motor speeds
+    # leftMotor1.setVelocity(error * 0.5)
+    # leftMotor2.setVelocity(error * 0.5)
+    # rightMotor1.setVelocity(error * 0.5)
+    # rightMotor2.setVelocity(error * 0.5)
+    i = 0
+
+
+
