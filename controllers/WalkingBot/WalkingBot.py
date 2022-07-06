761ca9b0fade2dd32d4f94cdeb95f6a14e378adc
Jannis Maier <jannismaier97@gmail.com>
2022-07-06 18:42:22 +0200

    supervisor and controller experiments WIP

--- controllers/WalkingBot/WalkingBot.py
+++ controllers/WalkingBot/WalkingBot.py
@@ -1,47 +1,51 @@
-from controller import Robot, Device, Node, Motor, Accelerometer
+from controller import Robot, Device, Node, Motor, Accelerometer, Supervisor
 
-# Global defines
-TIME_STEP = 32
-yTarget = 1
 
-# Robot initialization
-robot = Robot()
+class WalkingBot(Robot):
+    """Make the NAO robot run as fast as possible."""
+    m_timeStep: int
+    m_LeftMotor1: Motor
+    m_LeftMotor2: Motor
+    m_RightMotor1: Motor
+    m_RightMotor2: Motor
+
+    def initialize(self):
+        """Get device pointers, enable sensors and set robot initial pose."""
+        # This is the time step (ms) used in the motion file.
+        self.timeStep = 32
+        # Get pointers to the shoulder motors.
+        self.m_LeftMotor1 = self.getMotor('LeftLegMotor1')
+        self.m_LeftMotor2 = self.getMotor('LeftLegMotor2')
+        self.m_RightMotor1 = self.getMotor('RightLegMotor1')
+        self.m_RightMotor2 = self.getMotor('RightLegMotor2')
+
+
+    def run(self):
+        while True:
+            continue
+
+
+controller = WalkingBot()
+controller.initialize()
+controller.run()
+
+
 
 # For testing purposes
-numDevices = robot.getNumberOfDevices()
-print("Number of devices:", numDevices)
-for i in range(numDevices):
-    print("Device", i, ":", robot.getDeviceByIndex(i).getName())
+# numDevices = robot.getNumberOfDevices()
+# print("Number of devices:", numDevices)
+# for i in range(numDevices):
+#     print("Device", i, ":", robot.getDeviceByIndex(i).getName())
 
 
 # Motors
-leftMotor1 = robot.getDevice("LeftLegtMotor1")
-leftMotor2 = robot.getDevice("LeftLegtMotor2")
-rightMotor1 = robot.getDevice("RightLegtMotor1")
-rightMotor2 = robot.getDevice("RightLegtMotor2")
 
-# Set PID gains
-leftMotor1.setControlPID(100, 10, 0.1)
-leftMotor2.setControlPID(100, 10, 0.1)
-rightMotor1.setControlPID(100, 10, 0.1)
-rightMotor2.setControlPID(100, 10, 0.1)
 
 # Sensors
-accelerometer = robot.getDevice("accelerometer")
-accelerometer.enable(TIME_STEP)
+# accelerometer = robot.getDevice("accelerometer")
+# accelerometer.enable(TIME_STEP)
 
 # Main Loop to get the robot to walk
-while robot.step(TIME_STEP) != -1:
-    # Get robot y position
-    # y = accelerometer.getValues()[1]
-    # Calculate error
-    # error = abs(yTarget - y)
-    # Calculate motor speeds
-    # leftMotor1.setVelocity(error * 0.5)
-    # leftMotor2.setVelocity(error * 0.5)
-    # rightMotor1.setVelocity(error * 0.5)
-    # rightMotor2.setVelocity(error * 0.5)
-    i = 0
-
-
-
+# while robot.step(TIME_STEP) != -1:
+#     i = 0
+    # Read in sensordata 
